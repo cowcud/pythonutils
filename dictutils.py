7f253b75d471c1bb8d58cd7e30b95eef18fb62a4
cowcud <45083518+cowcud@users.noreply.github.com>
2018-11-16 10:02:11 +0800

    Add files via upload

---
+++ dictutils.py
@@ -0,0 +1,44 @@
+# Convert dict {k:v} into array of [k,v]'s
+def dict_to_array(dict,np=None):
+    """Converts dict {k:v} into array of [k,v]'s.
+
+    Parameters
+    ----------
+
+    dict:       dictionary
+        dict to convert.
+        
+    np:
+                If require numpy array result, pass imported numpy module.
+
+    Returns
+    ----------
+
+    array of arrays [[k1,v1],[k2,v2]...]
+    """
+    
+    res = ([[z[0],z[1]] for z in zip(dict.keys(),dict.values())])
+    if(np):
+        res = np.array(res)
+        
+    return res
+
+def dict_to_keys_values_arrays(dict,np=None):
+    """Converts dict {k:v} into dict with {'src':list of keys, 'trg':list of values}.
+
+    Parameters
+    ----------
+
+    dict:       dictionary
+        dict to convert.
+
+    Returns
+    ----------
+
+    dictionary {'src':[k1,k2,k3...], 'trg':[v1,v2,v3...]}
+    """
+    kv = dict_to_array(dict,np)
+    return {
+        "keys": kv[:,0],
+        "values": kv[:,1]
+    }
